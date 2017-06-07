7be8e945c122d8c09e54eae8d09d4e3ed4e2bc2d
[hgddzzcv] <[747692815@qq.com]>
2017-06-07 23:06:45 +0800

    6.7

--- app/src/main/java/com/ezworking/wechatunlock/adapter/OrderLvAdapter.java
+++ app/src/main/java/com/ezworking/wechatunlock/adapter/OrderLvAdapter.java
@@ -1,22 +1,37 @@
 package com.ezworking.wechatunlock.adapter;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.ezworking.my_android.base.utils.AsyncHttpClientUtil;
+import com.ezworking.my_android.base.utils.ToastUtil;
 import com.ezworking.wechatunlock.R;
+import com.ezworking.wechatunlock.api.ConstantNetUrl;
+import com.ezworking.wechatunlock.api.RequestApi;
 import com.ezworking.wechatunlock.domain.OrderResult;
+import com.ezworking.wechatunlock.domain.ResultBean;
+import com.google.gson.Gson;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
 
 /**
  * Created by wangchao on 2017/5/14 0014.
  */
 
-public class OrderLvAdapter<T> extends BaseAdapter {
+public class OrderLvAdapter<T> extends BaseAdapter implements View.OnClickListener {
 
     private static final String STATE_LOST_EFFORT = "-1";
 
@@ -90,18 +105,111 @@ public class OrderLvAdapter<T> extends BaseAdapter {
                 holder.stateButton.setText(context.getResources().getString(R.string.refuse));
                 break;
         }
+
+        holder.position = position;
+        holder.stateButton.setOnClickListener(this);
         return convertView;
     }
 
+    @Override
+    public void onClick(View v) {
+        View convertView = (View) v.getParent();
+        ViewHolder holder = (ViewHolder) convertView.getTag();
+        final int position = holder.position;
+        final String oID = orders.get(position).oID;
+        switch (v.getId()){
+            case R.id.order_state:
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setMessage("解锁订单");
+                builder.setPositiveButton("接受", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        requestState(oID,"0",position);
+                    }
+                });
+                builder.setNegativeButton("拒绝", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        requestState(oID,"1",position);
+                    }
+                });
+                builder.show();
+                break;
+        }
+    }
+
+    /**
+     *
+     * @param oID
+     * @param type  0 - 解锁 1-拒绝
+     */
+    public void requestState(String oID, final String type, final int position){
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("oID",oID);
+            jsonObject.put("type",type);
+            RequestApi.jsonPost(context, ConstantNetUrl.UNLOCKPHONE, jsonObject, new AsyncHttpResponseHandler() {
+
+                @Override
+                public void onStart() {
+                }
+
+                @Override
+                public void onFinish() {
+                }
+
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                    try {
+                        String response = new String(responseBody, "utf-8");
+                        ResultBean resultBean = new Gson().fromJson(response, ResultBean.class);
+                        if (resultBean.success.equals("0")) {
+                            Log.e("111","111" +resultBean.getErrorMsg());
+                            ToastUtil.showToast(context, resultBean.getErrorMsg());
+                            return;
+                        }
+
+                        if (resultBean!=null){
+                            if("0".equals(type)){
+                                orders.get(position).state = "1";
+                            }else if("1".equals(type)){
+                                orders.get(position).state = "2";
+                            }
+                        }
+                        notifyDataSetChanged();
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+                @Override
+                public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                    AsyncHttpClientUtil.onFailure(context, statusCode);
+                }
+            });
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+    };
+
     class ViewHolder{
 
+
+
         public ViewHolder(View convertView) {
+
             orderNumber = (TextView) convertView.findViewById(R.id.order_number);
             orderTiem = (TextView) convertView.findViewById(R.id.order_time);
             helpPhone = (TextView) convertView.findViewById(R.id.help_phone);
             stateButton = (Button) convertView.findViewById(R.id.order_state);
         }
 
+        int position;
         TextView orderNumber;
 
         TextView orderTiem;
